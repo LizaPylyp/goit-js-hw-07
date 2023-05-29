db3ec82dcd06f37fbc60c0928cb620f70eda0e51
Liza <lizkapylypchuk123456@gmail.com>
2023-05-29 21:51:33 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+  const galleryList = document.querySelector('.gallery');
+  function createGalleryItem(item) {
+    const galleryItem = document.createElement('li');
+    galleryItem.classList.add('gallery__item');
+    const link = document.createElement('a');
+    link.classList.add('gallery__link');
+    link.href = item.original;
+    const image = document.createElement('img');
+    image.classList.add('gallery__image');
+    image.src = item.preview;
+    image.alt = item.description;
+    image.setAttribute('data-source', item.original);
+    link.appendChild(image);
+    galleryItem.appendChild(link);
+    return galleryItem;
+  }
+  function renderGalleryItems(items) {
+    const galleryItems = items.map(item => createGalleryItem(item));
+    galleryList.append(...galleryItems);
+  }
+  renderGalleryItems(galleryItems);
+  console.log(galleryItems);
