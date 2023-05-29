5271f7eb3791138e9a7bccad95fbbac580e6b6b2
Liza <lizkapylypchuk123456@gmail.com>
2023-05-29 21:51:37 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+  const galleryList = document.querySelector('.gallery');
+  
+  function createGalleryItem(item) {
+    const galleryItem = document.createElement('li');
+    galleryItem.classList.add('gallery__item');
+  
+    const link = document.createElement('a');
+    link.classList.add('gallery__link');
+    link.href = item.original;
+
+    const image = document.createElement('img');
+    image.classList.add('gallery__image');
+    image.src = item.preview;
+    image.alt = item.description;
+    link.appendChild(image);
+    galleryItem.appendChild(link);
+    return galleryItem;
+  }
+  function renderGalleryItems(items) {
+    const galleryItems = items.map(item => createGalleryItem(item));
+    galleryList.append(...galleryItems);
+  }
+  renderGalleryItems(galleryItems);
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+  });  
 console.log(galleryItems);
