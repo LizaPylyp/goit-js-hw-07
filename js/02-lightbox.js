8a32086e816b04b9727c2be41fb28339ca2783c3
Liza <lizkapylypchuk123456@gmail.com>
2023-06-05 21:58:34 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,30 +1,49 @@
+
+// Change code below this line
 import { galleryItems } from './gallery-items.js';
 
-  const galleryList = document.querySelector('.gallery');
-  
-  function createGalleryItem(item) {
-    const galleryItem = document.createElement('li');
-    galleryItem.classList.add('gallery__item');
-  
-    const link = document.createElement('a');
-    link.classList.add('gallery__link');
-    link.href = item.original;
+const galleryList = document.querySelector('.gallery');
 
-    const image = document.createElement('img');
-    image.classList.add('gallery__image');
-    image.src = item.preview;
-    image.alt = item.description;
-    link.appendChild(image);
-    galleryItem.appendChild(link);
-    return galleryItem;
+function createGalleryItem(item) {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.href = item.original;
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = item.preview;
+  image.alt = item.description;
+  image.setAttribute('data-source', item.original);
+
+  link.appendChild(image);
+  galleryItem.appendChild(link);
+
+  return galleryItem;
+}
+
+function openModal(event) {
+  event.preventDefault();
+
+  const target = event.target;
+  if (target.classList.contains('gallery__image')) {
+    const imageSource = target.dataset.source;
+
+    const instance = basicLightbox.create(`<img src="${imageSource}">`);
+    instance.show();
   }
-  function renderGalleryItems(items) {
-    const galleryItems = items.map(item => createGalleryItem(item));
-    galleryList.append(...galleryItems);
-  }
-  renderGalleryItems(galleryItems);
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250
-  });  
+}
+
+galleryList.addEventListener('click', openModal);
+
+function renderGalleryItems(items) {
+  const galleryItems = items.map((item) => createGalleryItem(item));
+  galleryList.append(...galleryItems);
+}
+
+renderGalleryItems(galleryItems);
 console.log(galleryItems);
+
+
