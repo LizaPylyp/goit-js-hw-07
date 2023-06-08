9b1b6d92dc80cc3bcdeb805dfc01cae362c19561
Liza <lizkapylypchuk123456@gmail.com>
2023-06-08 21:48:35 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,25 @@
 
 // Change code below this line
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
+const myGalleryUlEl = document.querySelector(".gallery");
+const galleryMarkup = galleryItems
+  .map(
+    ({ original, preview, description }) => `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>`
+  )
+  .join("");
+
+myGalleryUlEl.innerHTML = galleryMarkup;
 
 
-console.log(galleryItems);
-
-
+   let   lightbox = new SimpleLightbox(".gallery a", {
+      sourceAttr: "href",
+      captionDelay: 250,
+      captionSelector: "img",
+      captionsData: "alt",
+    });
+ 
